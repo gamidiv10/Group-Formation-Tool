036b9b84dc76c91058e471d72bf7493cf2bba0be
Vamsi Gamidi <vm709690@dal.ca>
2020-05-29 23:41:11 -0300

    Authentication Initial Commit

--- src/main/java/com/advsdc/group2/login/controller/LoginController.java
+++ src/main/java/com/advsdc/group2/login/controller/LoginController.java
@@ -5,9 +5,8 @@ import com.advsdc.group2.login.dao.LoginDaoImpl;
 import com.advsdc.group2.login.models.UserCredentials;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -17,22 +16,27 @@ import java.sql.Statement;
 @Controller
 public class LoginController {
 
-    @GetMapping("/login")
-    public String loginForm(Model model)
+    @RequestMapping("/login.html")
+    public String loginForm()
     {
-        model.addAttribute("userCred", new UserCredentials());
-        return "login";
+        return "login.html";
     }
 
-    @PostMapping("/login")
-    public String loginSubmit(@ModelAttribute UserCredentials userCred)
-    {
-        UserCredentials userCredFromDb = new UserCredentials();
-        LoginDaoImpl ld = new LoginDaoImpl();
-        userCredFromDb = ld.getUserCredentials(userCred.getUserId());
-        if(userCredFromDb.getUserId() != null) {
-            return "result";
-        }
-        return null;
+    @RequestMapping("/login-error.html")
+    public String loginError(Model model) {
+        model.addAttribute("loginError", true);
+        return "login.html";
     }
+//    @PostMapping("/login")
+//    public String loginSubmit(@ModelAttribute UserCredentials userCred)
+//    {
+//        UserCredentials userCredFromDb = new UserCredentials();
+//        LoginDaoImpl ld = new LoginDaoImpl();
+//        userCredFromDb = ld.getUserCredentials(userCred.getUserId());
+//        if(userCredFromDb.getUserId() != null) {
+//            return "result";
+//        }
+//        return null;
+//    }
+
 }
---
+++ src/main/java/com/advsdc/group2/security/SecurityConfiguration.java
@@ -0,0 +1,34 @@
+package com.advsdc.group2.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers( "/public/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login.html")
+                .failureUrl("/login-error.html")
+                .permitAll();
+    }
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("{noop}pass")
+                .roles("USER");
+    }
+}
