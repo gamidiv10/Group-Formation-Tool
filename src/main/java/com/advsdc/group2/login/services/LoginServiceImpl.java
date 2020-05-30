79da5b004c99ec1f59184fc75fc4bb1e500992ec
Vamsi Gamidi <vm709690@dal.ca>
2020-05-30 14:20:02 -0300

    Password Verification

--- src/main/java/com/advsdc/group2/login/services/LoginServiceImpl.java
+++ src/main/java/com/advsdc/group2/login/services/LoginServiceImpl.java
@@ -2,17 +2,17 @@ package com.advsdc.group2.login.services;
 
 import com.advsdc.group2.login.dao.LoginDaoImpl;
 import com.advsdc.group2.login.models.UserCredentials;
+import com.advsdc.group2.security.PasswordEncryption;
 
 public class LoginServiceImpl {
     private String passwordFromDb;
     public boolean validateUser(UserCredentials userCred){
         LoginDaoImpl loginDao = new LoginDaoImpl();
+
         passwordFromDb = loginDao.getUserCredentials(userCred.getUserId());
-        if(passwordFromDb != null) {
-            return passwordFromDb.equals(userCred.getPassword());
-        }
-        else{
-            return false;
-        }
+        PasswordEncryption passwordEncryption = new PasswordEncryption();
+        return passwordEncryption.matches(userCred.getPassword(), passwordFromDb);
+
+
     }
 }
