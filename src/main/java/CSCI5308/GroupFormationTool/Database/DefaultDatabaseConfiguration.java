5a28b76f8956eeae5b0a3edf96fcdd77506c0807
Vamsi Gamidi <vm709690@dal.ca>
2020-06-18 18:03:09 -0300

    Changed DB Variables

--- src/main/java/CSCI5308/GroupFormationTool/Database/DefaultDatabaseConfiguration.java
+++ src/main/java/CSCI5308/GroupFormationTool/Database/DefaultDatabaseConfiguration.java
@@ -4,14 +4,12 @@ public class DefaultDatabaseConfiguration implements IDatabaseConfiguration
 {
 	
 	
-	private static final String URL = "jdbc:mysql://(host=db-5308.cs.dal.ca,port=3306)/CSCI5308_2_TEST?serverTimezone=AST";
-	private static final String USER = "CSCI5308_2_TEST_USER";
-	private static final String PASSWORD = "CSCI5308_2_TEST_2440";
-	/*
-	 * private static final String URL = System.getenv("URL"); private static final
-	 * String USER = System.getenv("USER"); private static final String PASSWORD =
-	 * System.getenv("PASSWORD");
-	 */
+
+	 private static final String URL = System.getenv("URL");
+	 private static final String USER = System.getenv("USER");
+	  private static final String PASSWORD =
+	 System.getenv("PASSWORD");
+
 
 	public String getDatabaseUserName()
 	{
